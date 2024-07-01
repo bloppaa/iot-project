3e1ea881763f1657eb2cf8603f51bba1147dfe31
blopa <pablocortes1101@gmail.com>
2024-07-01 02:21:20 -0400

    feat: add dynamic rendering via websockets

--- server/mqttClient.js
+++ server/mqttClient.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const mqtt = require('mqtt')
 const client = mqtt.connect({
   host: process.env.MQTT_HOST,
   port: process.env.MQTT_PORT,
-})
-const topic = process.env.TOPIC
+});
+const topic = process.env.TOPIC;
 
 client.on('connect', () => {
   client.subscribe(topic, (err) => {
@@ -15,10 +15,6 @@ client.on('connect', () => {
       console.error(err)
     }
   })
-})
+});
 
-client.on('message', (topic, message) => {
-  console.log(message.toString())
-})
-
-module.exports = client
+module.exports = client;
