3fff8b92a88b19c1c18ef42e60d30bcca419a6a4
blopa <pablocortes1101@gmail.com>
2024-07-01 01:56:24 -0400

    Add dashboard webpage with placeholder data

---
+++ server/publisher.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const { SerialPort } = require('serialport');
+const { ReadlineParser} = require('@serialport/parser-readline');
+const mqtt = require('mqtt');
+
+const port = new SerialPort({path: 'COM3', baudRate: 115200, });
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+const client = mqtt.connect({
+  host: process.env.MQTT_HOST,
+  port: process.env.MQTT_PORT,
+});
+const topic = process.env.TOPIC;
+
+client.on('connect', () => {
+  console.log('Conectado al broker MQTT');
+});
+
+parser.on('data', (data) => {
+  const height = data.trim();
+  client.publish(topic, height);
+  console.log(`Altura publicada: ${height}`);
+});
+
+port.on('open', () => {
+  console.log('Puerto serial abierto');
+});
+
+port.on('error', (err) => {
+  console.error('Error en el puerto serial:', err.message);
+})
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require("express")
+const express = require('express');
 const http = require('http')
 const WebSocket = require('ws')
 const mqttClient = require('./mqttClient')
+const path = require('path');
 
 const app = express()
 const port = 3000
@@ -19,8 +20,12 @@ mqttClient.on('message', (topic, message) => {
   })
 })
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+  res.render('index');
 })
 
 wss.on('connection', (ws) => {
